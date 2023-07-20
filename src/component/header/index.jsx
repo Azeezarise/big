3ba6f228c07abddcbb025e7e6cd6e88c61e4a544
azeezarise <azeezarise@gmail.com>
2023-07-20 13:57:22 +0100

    added header component

---
+++ src/component/header/index.jsx
@@ -0,0 +1,22 @@
+import { Wrapper, LogoImg, Content, TMDBImg } from "./Header.styles"
+import RMDBlogo from '../../images/react-movie-logo.svg'
+import TMDBLogo from '../../images/tmdb_logo.svg';
+
+
+
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <Content>
+                <a href="#">
+                    <LogoImg src={RMDBlogo} alt="rmdb-logo" />
+                </a>
+
+
+                <TMDBImg src={TMDBLogo} alt="tmdb-logo"></TMDBImg>
+            </Content>
+        </Wrapper>
+    )
+}
+export default Header
