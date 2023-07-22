1561bb1ef2f3438bb4d79341d7d5277b889f1ec9
azeezarise <azeezarise@gmail.com>
2023-07-22 15:58:54 +0100

    added backround

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Thumb image={"https://cdn.vox-cdn.com/thumbor/DjZeeL2WELkl_OuCiou2AOG-XUY=/133x0:2775x1321/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/24725191/trotbfp132k2.jpg"} movieId={1} clickable={true} />
         <Thumb image={"https://cdn.vox-cdn.com/thumbor/DjZeeL2WELkl_OuCiou2AOG-XUY=/133x0:2775x1321/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/24725191/trotbfp132k2.jpg"} movieId={1} clickable={true} />
         <Thumb image={"https://cdn.vox-cdn.com/thumbor/DjZeeL2WELkl_OuCiou2AOG-XUY=/133x0:2775x1321/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/24725191/trotbfp132k2.jpg"} movieId={1} clickable={true} />
-        <Thumb image={"https://cdn.vox-cdn.com/thumbor/DjZeeL2WELkl_OuCiou2AOG-XUY=/133x0:2775x1321/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/24725191/trotbfp132k2.jpg"} movieId={1} clickable={true} />
+        <Thumb image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU_3H3FTGmIDIU9oW4n1lJIxKCBOxMzDG2HQ&usqp=CAU"} movieId={1} clickable={true} />
 
       </Grid>
       <GlobalStyle />
