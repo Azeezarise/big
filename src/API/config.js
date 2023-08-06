a55b59f6cf6892fb9ef75dc8d33c053cc26ab6cb
azeezarise <azeezarise@gmail.com>
2023-08-06 17:39:32 +0100

    added loaded image

--- src/API/config.js
+++ src/API/config.js
@@ -1,5 +1,5 @@
 export const API_URL= 'https://api.themoviedb.org/3/'
-export const API_KEY= 'fcad98126bff1f84303faa83c74805c8'
+export const API_KEY= '0cbce697e799141de84bc9fc35437d5e'
 export const SEARCH_BASE_URL= `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=`
 export const POPULAR_BASE_URL= `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US`
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
